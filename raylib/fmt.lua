a730383285dcceaba37b5376c64de37afae85df8
ElNico56 <meeqnico@gmail.com>
2025-03-28 08:06:15 -0300

    cleanup

--- raylib/fmt.lua
+++ raylib/fmt.lua
@@ -22,29 +22,6 @@ local function intPart(x)
 	return ret
 end
 
-local reset = "[0m"
-function LambdaToString(expr, depth)
-	depth = depth or 0
-	if depth > 20 then return "!!!" end
-	if type(expr) == "table" then
-		if expr[1] == L then
-			local col = hsvANSI(intPart(depth), 0.8, 0.9)
-			local v = col..char((depth) % 26 + 97)..reset
-			local a = LambdaToString(expr[2], depth + 1)
-			return ("\\%s.%s"):format(v, a)
-		end
-		local left = LambdaToString(expr[1], depth)
-		local right = LambdaToString(expr[2], depth)
-		return ("(%s %s)"):format(left, right)
-	end
-	if not expr then return "[31mNIL[0m" end
-	local col = hsvANSI(intPart(depth - expr), 0.8, 0.9)
-	if depth < expr then
-		col = ("[38;2;%d;%d;%dm"):format(255 / (expr - depth), 255 / (expr - depth), 255 / (expr - depth))
-	end
-	return col..char((depth - expr) % 26 + 97)..reset
-end
-
 local function _str(expr, depth)
 	if not expr then return "NIL" end
 	if depth > 20 then return "!!!" end
@@ -62,6 +39,7 @@ local function _str(expr, depth)
 	end
 end
 
+local reset = "[0m"
 function Stringify(expr, color)
 	if color then
 		return (_str(expr, 0):gsub("[a-z]", function(letter)
